976db98d7f12a5bfe8b78f2f9a3307a7dda51986
Robin Wahlig <58917262+robin-whg@users.noreply.github.com>
2022-01-13 20:10:34 +0100

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>henlo, frien.</p>
+  <hello-world msg="test" />
+  <i-heroicons-outline-beaker class="h-6 w-6" />
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// import path from 'path'
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import Layouts from "vite-plugin-vue-layouts";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+import Icons from "unplugin-icons/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // resolve: {
+  // alias: {
+  //   '~/': `${path.resolve(__dirname, 'src')}/`,
+  // },
+  // },
+  plugins: [
+    Vue(),
+    Pages(),
+    Layouts(),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
+    Icons(),
+  ],
+});
